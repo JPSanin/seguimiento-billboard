b45cfa389c80e623ff6f6d0ee1e5d1e0fa2b5ddf
Juan Sanin <juanpablosaninb@gmail.com>
2020-08-28 15:38:56 -0500

    Infrastructure class development in model package

--- src/model/InfrastructureDepartment.java
+++ src/model/InfrastructureDepartment.java
@@ -1,5 +1,38 @@
 package model;
+import java.util.*;
 
 public class InfrastructureDepartment {
 
+	private static final String BILLBOARD_FILE_NAME = "data/billboard.bbd";
+	private ArrayList<Billboard> billboards;
+	
+	public InfrastructureDepartment() {
+		billboards= new ArrayList<Billboard>();
+		loadBillboard();
+	}
+	
+	public void addBillboard(double w, double h, boolean iu, String b ) {
+		saveBillboard();
+	}
+	
+	//Usar en addBillboard
+	private void saveBillboard(){}
+	
+	//Usar en el constructor
+	private void loadBillboard() {}
+	
+	public void exportDangerousBillboardReport(String fn) {}
+	
+	public void importData(String fn) {}
+	
+	@Override
+	public String toString() {
+		String info="";
+		return info;
+	}
+	
+	
+	
+	
+	
 }
